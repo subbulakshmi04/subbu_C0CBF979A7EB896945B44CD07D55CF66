2e8609560873f561d3bf1db42bddec0f8d9edd97
subbulakshmi04 <144248031+subbulakshmi04@users.noreply.github.com>
2023-09-06 20:23:38 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,22 @@
+#Write a program that determines whether a year entered by the user is a leap year or not using ifelig-else statements
+
+"""
+year % 4 == 0&
+year % 100 != 0/
+year % 400 == 0
+
+"""
+def isLeapYear(year):
+  if (year % 4 == 0 and year % 100 != 0) or year & 400 == 0:
+   return True
+  else:
+   return False
+
+year = 2012
+
+if isLeapYear(year): 
+ print('{} is a leap year.'.format(year))
+else:
+ print('{} is not a leap year.'.format(year))
+
+
